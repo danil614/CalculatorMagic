c575d91a14b7cc30e05ed139a42180a4c9c1cd67
Danil <fs159@mail.ru>
2020-10-04 20:17:26 +0300

    Update

--- src/simple_calculator.py
+++ src/simple_calculator.py
@@ -6,11 +6,10 @@ class Calculator:
         if len(self.__dict__) <= 10:
             self.__dict__[item] = value
         else:
-            print('10 attributes already created')
+            raise AttributeError('10 attributes already created')
 
     def __iter__(self):
-        print(self.value)
-        return self
+        yield self.value
 
     def __add__(self, other):
         value = other.value if isinstance(other, Calculator) else other
@@ -89,21 +88,5 @@ class Calculator:
 
 
 if __name__ == '__main__':
-    # calculator = Calculator(5)
-    # calculator.hhh = 1
-    # calculator.ll = 1
-    # calculator.ppp = 1
-    # calculator.jjj = 1
-    # calculator.ttt = 1
-    # calculator.ggg = 1
-    # calculator.lll = 1
-    # calculator.ttt = 1
-    # calculator.www = 1
-    # calculator.yyy = 1
-    # calculator.ooo = 1
-    # calculator.uuu = 1
-
-    # print(calculator)
-    # print(calculator.add(1, 2, 3, 5.1).multiply(4, 0.123).subtract(4, 1, -100).divide(5, integer_divide=True))
     print((Calculator(3) ** 3) + 4)
     print(3 ** 3)
